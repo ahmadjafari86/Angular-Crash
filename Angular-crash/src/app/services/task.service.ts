5997e3662eda0170c1c0732c6c3bff6c367483a1
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-09-29 22:35:33 +0330

    commit_3

--- Angular-crash/src/app/services/task.service.ts
+++ Angular-crash/src/app/services/task.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { Observable,of } from 'rxjs';
+import { Task } from 'src/app/task';
+import { TASKS } from 'src/app/mock-tasks';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
   constructor() { }
+  getTasks():Observable<Task[]> {
+     const tasks = of(TASKS);
+     return tasks;
+  }
 }
