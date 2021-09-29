12bc53099e8ec7a6a4820dba42f639546aa7eb03
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-09-29 11:36:21 +0330

    commit_1

---
+++ Angular-crash/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/task';
+import { TASKS } from 'src/app/mock-tasks';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TaskComponent implements OnInit {
+  tasks:Task[]=TASKS;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
